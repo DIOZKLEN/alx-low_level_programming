82675e43edafda514e84076206e5b11da0d64384
DIOZKLEN <madiopdioufouza.diallo@uvs.edu.sn>
2023-03-16 09:51:05 -0700

    print char with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,16 @@
 #include <stdio.h>
 
-int main(void) { puts("Programming is like building a multilingual puzzle\n"); return (0); }
+
+/**
+ * main-print 
+ * print("Programming is like building a multilingual puzzle)
+ * return : 0
+ */
+
+
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
