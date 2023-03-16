f2917548b0812988697ee4d6ce18612dd970ed9a
DIOZKLEN <madiopdioufouza.diallo@uvs.edu.sn>
2023-03-16 10:45:43 -0700

    size of the types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/**
+ * main - print
+ * print: all size 
+ * Return: Always 0.
+ */
+
+int main()
+
+{
+	printf("Size of char: %2d bytes \n", sizeof(char));
+	printf("Size of int: %2d bytes \n", sizeof(int));
+	printf("Size of long int: %2d bytes \n", sizeof(long int));
+	printf("Size of long long int: %2d bytes \n", sizeof(long long int));
+	printf("Size of float: %2d bytes \n", sizeof(float));
+	return (0);
+}
