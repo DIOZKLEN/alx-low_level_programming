91e494a1208b61aa9cf7bbe4b370eb23f10ee1e0
DIOZKLEN <madiopdioufouza.diallo@uvs.edu.sn>
2023-03-17 16:03:51 -0700

    print nbmre

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - for loop printdigit
+ * Return: 0.
+ *
+ */
+
+int main(void)
+
+{
+	char dio;
+
+	for (dio = '0'; dio <= '9'; dio++)
+	putchar(dio)
+	putchar('\n')
+	return (0);
+}
