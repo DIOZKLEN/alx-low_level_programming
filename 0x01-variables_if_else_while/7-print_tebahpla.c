9e6783328fc0d98db43ad8d81091dfaf7c7e591b
DIOZKLEN <madiopdioufouza.diallo@uvs.edu.sn>
2023-03-17 16:28:18 -0700

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - for loop 
+ * reverse alphabet
+ * Return: 0.
+ */
+
+int main(void)
+
+{
+	char dio;
+
+	for (dio = 'z' ; dio >= 'a'; dio--)
+	putchar(dio);
+	putchar('\n');
+	return (0);
+}
