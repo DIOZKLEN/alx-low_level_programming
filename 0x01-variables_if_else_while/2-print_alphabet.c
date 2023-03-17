885ae054d6663b261c2bba155b862c1294da2c36
DIOZKLEN <madiopdioufouza.diallo@uvs.edu.sn>
2023-03-17 13:19:39 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - for loop
+ * ecrire alphabet
+ * Return: 0.
+ */
+
+int main()
+
+{
+	char dio;
+	for (dio = 'a'; dio <= 'z'; ++dio)
+	{
+		putchar(dio);
+	}
+	return (0);
+
+}
