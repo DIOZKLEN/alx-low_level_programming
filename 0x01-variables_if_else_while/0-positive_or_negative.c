055764e9275a1aaa7fbe9d7a3492e49b95160c81
DIOZKLEN <madiopdioufouza.diallo@uvs.edu.sn>
2023-03-17 09:32:44 -0700

    if condition

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,37 +1,31 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
 
 /**
- * main - if fonction
- * "write fonction that return
- * aleatory number"
- * Return: Always (Success).
+ * main - if condition positive
+ * negative
+ * Return: Always (Success)
  */
 
-
 int main(void)
- 
 {
 	int n;
 
-
 	srand(time(0));
-	n = rand() - RAND_MAX /2;
-
-        if (n > 0)
-       	{
-                printf("%d is positive\n", n);
-        }       
-        else if (n == 0)
-       	{
-                printf("%d is zero\n", n);
-        }       
-        else 
-       	{
-                printf ("%d is negative\n", n);
-        }       
-        return (0);
+	n = rand() - RAND_MAX / 2;
+	
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
 }
