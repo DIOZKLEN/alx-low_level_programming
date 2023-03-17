3831167376de3a4d865d1a8281331149f93ea783
DIOZKLEN <madiopdioufouza.diallo@uvs.edu.sn>
2023-03-17 16:21:27 -0700

    print nbre decimal

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - for loop digit
+ * without char
+ * Return: (Success)
+ */
+
+int main(void)
+
+{
+	int dio;
+
+	for (dio = '1'; dio <= '9'; dio++)
+	putchar(dio);
+	putchar('\n');
+	return (0);
+}
