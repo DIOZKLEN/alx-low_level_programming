3aeadc00b5771ea2c4a3e8548789c3ba7c30e13d
DIOZKLEN <madiopdioufouza.diallo@uvs.edu.sn>
2023-03-21 07:17:20 -0700

    alphabet with another void

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet another
+ * void
+ * Return: Success
+ */
+
+void print_alphabet(void);
+
+{
+	char dio;
+
+	for (dio = 'a', dio <= 'z', dio++)
+	putchar(dio);
+	putchar('\n');
+}
+
+int main(void)
+
+{
+	print_alphabet();
+	return (0);
+}
