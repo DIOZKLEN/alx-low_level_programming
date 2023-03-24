41418903af058095c596283ab8a096aaa47c86aa
DIOZKLEN <madiopdioufouza.diallo@uvs.edu.sn>
2023-03-23 21:12:14 -0700

    upper check

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - if condition when 
+ * isupper return 1 else 0
+ * Return: Success
+ */
+
+int _isupper(int c)
+
+{
+	if (!(c >= 'A' && c >= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
