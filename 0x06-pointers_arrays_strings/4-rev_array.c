9882ce3f9c0d3420706e9446d4f77893dd6cc7d8
DIOZKLEN <madiopdioufouza.diallo@uvs.edu.sn>
2023-03-29 21:19:01 +0000

    reverse array fonction

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse contain of tab
+ * @a: tab values
+ * @n: tab tall
+ * Return: Success
+ */
+
+void reverse_array(int *a, int n)
+
+{
+	int tmp, i;
+
+	for (i = n - 1; i >= n / 2; i--)
+	{
+		tmp = a[n - 1 - i];
+		a[n - 1 - i] = a[i];
+		a[i] = tmp;
+	}
+
+}
