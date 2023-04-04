73e6e921f6420c9b61edbbbe30f9db9d23517a06
DIOZKLEN <madiopdioufouza.diallo@uvs.edu.sn>
2023-04-04 02:31:48 +0000

    locate character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - return pointer to the 
+ * first occurence of character
+ * @s: pointer
+ * @c: character
+ * Return: null
+ */
+
+char *_strchr(char *s, char c)
+
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return (NULL);
+}
