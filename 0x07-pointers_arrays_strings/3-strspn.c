1805929203298609f627b00c3b464154f8e884b1
DIOZKLEN <madiopdioufouza.diallo@uvs.edu.sn>
2023-04-04 16:17:57 +0000

    get len advanced

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strspn - return nbr of bytes in the
+ * initial segment
+ * @s: pointer
+ * @accept: pointer
+ * Return: success
+ */
+
+unsigned int _strspn(char *s, char *accept)
+
+{
+	unsigned int i, j, count = 0, check;
+
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		check = 0;
+
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				count++;
+				check = 1;
+			}
+		}
+		if (check == 0)
+			return(count);
+	}
+	return (count);
+}
