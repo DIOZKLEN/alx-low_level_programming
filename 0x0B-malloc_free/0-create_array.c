6c40319c8b808351a5d4572d5c764b61e3ed269d
DIOZKLEN <madiopdioufouza.diallo@uvs.edu.sn>
2023-04-11 22:40:10 +0000

    first malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *create_array - create array of char
+ * @size: input
+ * @c: character
+ * Return: NULL if size = 0, SUCCESS or NULL if that fail
+ */
+
+char *create_array(unsigned int size, char c)
+
+{
+	char *s;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	s = malloc(size * sizeof(char));
+
+	if (s == NULL)
+		return(NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
+	return (s);
+}
