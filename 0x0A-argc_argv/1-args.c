c66a38184431e0a33b906627868b45a1fd75268c
DIOZKLEN <madiopdioufouza.diallo@uvs.edu.sn>
2023-04-15 17:15:39 +0000

    number of args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,19 +1,15 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * main - print number of args
+ * main - Print number of args
  * @argc: count args
  * @argv: char args
  * Return: 0
  */
 
 int main(int argc, char *argv[])
-
 {
 	(void) argv;
-
 	printf ("%d\n", argc - 1);
-
 	return (0);
 }
