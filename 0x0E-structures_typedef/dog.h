4a0d98f23bae50eca386f27335fd76acbddeb2ea
DIOZKLEN <madiopdioufouza.diallo@uvs.edu.sn>
2023-04-18 00:30:28 +0000

    my first struct with poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * struct dog - creat my first struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: ...
+ * Description: i will create a new struct
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
