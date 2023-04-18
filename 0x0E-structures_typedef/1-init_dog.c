19c7f3bc9ae13373b4e57805dc8d5613a4b32985
DIOZKLEN <madiopdioufouza.diallo@uvs.edu.sn>
2023-04-18 21:50:29 +0000

    initialize struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,16 +2,16 @@
 #include <stddef.h>
 
 /**
- * init_dog - initialize a variable of type struct dog
+ * init_dog - Initialize struct in C
  * @d: struct
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
- *
- * Return: nothing
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: Success
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
+
 {
 	if (d != NULL)
 	{
