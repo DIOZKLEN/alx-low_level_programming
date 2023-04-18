be62c5ceb859b20e2898b6a77c9d6f564b6687ea
DIOZKLEN <madiopdioufouza.diallo@uvs.edu.sn>
2023-04-18 21:42:24 +0000

    initialize struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,4 +17,12 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - typedef for dog structure
+ */
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif
