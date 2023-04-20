e1f15dcbd3a9b66162a34e81c57276f4ca524dd6
DIOZKLEN <madiopdioufouza.diallo@uvs.edu.sn>
2023-04-20 00:12:04 +0000

    searches for an integer.

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * int_index - searches for an integer
+ * @array: the array
+ * @size: size of array
+ * @cmp: the pointer
+ *
+ * Return: Success
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+
+{
+	int i;
+
+	if (array != NULL && size > 0 && cmp != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{	
+			if (cmp(array[i]))
+				return (i);
+		}
+	}
+	return (-1);
+}
