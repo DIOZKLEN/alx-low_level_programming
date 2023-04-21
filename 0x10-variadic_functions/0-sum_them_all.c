96c32deccdb1ecb7b78a8a3c57c6c5c76f6c0ae9
DIOZKLEN <madiopdioufouza.diallo@uvs.edu.sn>
2023-04-21 00:04:29 +0000

    returns the sum of all its parameters.

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,11 +14,11 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i, sum = 0;
 
 	va_start(dio, n);
+
 	for (i = 0; i < n; i++)
 		sum += va_arg(dio, int);
 
 	va_end(dio);
 
 	return (sum);
-	
 }
