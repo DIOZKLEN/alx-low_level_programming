a7936e5e74070502e030e13c7c55cbd2903d0dba
DIOZKLEN <madiopdioufouza.diallo@uvs.edu.sn>
2023-06-07 20:36:01 +0000

    wildcmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * wildcmp - compare strings
+ * @s1: str1
+ * @s2: str2,can contain special char
+ * Return: 1 if success, 0 otherwise
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return (0);
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+
+	return (0);
+}
